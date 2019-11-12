76dc413149eba0b0ac5dbf0bf147971429dec83e
marinooy <marianooy@hotmail.com>
2019-11-12 14:14:40 -0600

    Class modified to execute cucumber-selenium test

--- src/test/java/holapepino/RunCucumberTest.java
+++ src/test/java/holapepino/RunCucumberTest.java
@@ -5,7 +5,14 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber/wikipedia.json",
+                "html:target/cucumber/wikipedia-html",
+                "pretty"
+        },
+        tags = {"not @tag"}
+)
 
 
 public class RunCucumberTest {
